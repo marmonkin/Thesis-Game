35e368a7c97deb79461709c2d04ae7f4c429327a
Maksim Vlasov <89015773+marmonkin@users.noreply.github.com>
2026-07-17 01:37:47 +0300

    started switching cameras to phantom cameras

--- Scripts/camera/cam_anchor.gd
+++ Scripts/camera/cam_anchor.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 @export var rotation_speed = 1.
 @export var move_speed = 5.
-@export var rotation_deg = 1
+@export var rotation_deg = 45.
 
 var target_rotation = .0
 var move_target = self
@@ -10,13 +10,26 @@ var move_target_pos: Vector3
 
 var lock_rot = false
 
+
+@onready var pcam: PhantomCamera3D = $Camera3D
+
 func _ready() -> void:
-	target_rotation = rotation.y
+	target_rotation = pcam.get_third_person_rotation_degrees().y
 	RoomManager.register_camera(self)
+	
 
 
 func _process(delta):
-	rotation.y = lerp_angle(rotation.y, target_rotation, rotation_speed * delta)
+	var rot = pcam.get_third_person_rotation_degrees()
+	
+	rot.y = lerp_angle(
+		rot.y,
+		target_rotation,
+		rotation_speed * delta
+	)
+	
+	pcam.set_third_person_rotation_degrees(rot)
+	
 	global_position = lerp(global_position, move_target_pos, move_speed * delta)
 
 
@@ -27,5 +40,8 @@ func move_to_room(room: Node3D):
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("rot_left") and not lock_rot:
 		target_rotation -= deg_to_rad(rotation_deg)
+		target_rotation = wrapf(target_rotation, 0.0, 360.0)
+		
 	if event.is_action_pressed("rot_right") and not lock_rot:
 		target_rotation += deg_to_rad(rotation_deg)
+		target_rotation = wrapf(target_rotation, 0.0, 360.0)
